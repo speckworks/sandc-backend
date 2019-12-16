6bf087055373b25103c5dc0416b0e9247d5903a6
John Speck <speckworks@gmail.com>
2019-12-16 15:27:41 -0500

    backend seeds persist

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,22 +9,21 @@
 u1 = User.create(name:"harold")
 r1 = Review.create(review:"It was freaking Aaaawesome.")
 
-# puts 'Fetching Beach Data...'
-# (1..1500).each do |beach|
-#   request_url = "https://api.coastal.ca.gov/access/v1/locations"
-#   request = RestClient::Request.execute(
-#     method: :get,
-#     url: request_url)
-
-#   beach = JSON.parse(request)
-# byebugexit
-#   Favbeach.create(
-#     name: beach.NameMobileWeb,
-#     img: beach.Photo_1,
-#     lat: beach.LATITUDE,
-#     lng: beach.LONGITUDE,
-#   )
-
-# end
+puts 'Fetching Beach Data...'
+request_url = "https://api.coastal.ca.gov/access/v1/locations"
+request = RestClient::Request.execute(
+  method: :get,
+  url: request_url)
+  
+  beaches = JSON.parse(request)
+    
+  beaches.each do |beach|
+  Favbeach.create(
+    name: beach["NameMobileWeb"],
+    img: beach["Photo_1"],
+    lat: beach["LATITUDE"],
+    lng: beach["LONGITUDE"]
+  )
+end
 
 puts "User, FavBeaches and Review are Good To Go."
